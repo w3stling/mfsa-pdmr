c83662791dd293a80f3cb17be5f730a18bb9d0d5
w3stling <w3stling@users.noreply.github.com>
2025-10-08 05:21:36 +0000

    Publishing javadoc

--- javadoc/2.0.9-SNAPSHOT/package-search-index.js
+++ javadoc/2.0.9-SNAPSHOT/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.apptasticsoftware.mfsapdmr","l":"com.apptasticsoftware.mfsapdmr"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"com.apptasticsoftware.mfsapdmr","l":"com.apptasticsoftware.mfsapdmr"}];updateSearchResults();
