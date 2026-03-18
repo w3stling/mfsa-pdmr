2325a2c73f93eb04b3684f8e54c5f4c17a92dabd
w3stling <w3stling@users.noreply.github.com>
2026-03-18 06:01:00 +0000

    Publishing javadoc

---
+++ javadoc/2.0.10-SNAPSHOT/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.apptasticsoftware.mfsapdmr","l":"PdmrRegistry"},{"p":"com.apptasticsoftware.mfsapdmr","l":"Transaction"}];updateSearchResults();
